c475ae101f3451da3b87e2f777bb5b51bfabba4c
glennlopez <glennlopez@gmail.com>
2016-07-01 11:15:32 -0700

    hello world completed

--- Tutorials/ProgrammingKnowledge/HelloWorld/main.cpp
+++ Tutorials/ProgrammingKnowledge/HelloWorld/main.cpp
@@ -1,2 +1,16 @@
 // https://www.youtube.com/watch?v=5JVLO8yBMXA&index=3&list=PLS1QulWo1RIZiBcTr5urECberTITj7gjA
 //@3:04
+
+#include<QApplication>
+#include<QLabel>
+
+int main(int argc, char* argv[]){
+    QApplication app(argc, argv);
+    QLabel *label = new QLabel("Hello World");
+
+    label->setWindowTitle("Glenn Application");
+    label->resize(400,400);
+    label->show();
+
+    return app.exec();
+}
